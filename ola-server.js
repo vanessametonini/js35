4f5f3fedb30080c568d7701aec9b7bc565750f0f
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-12-08 18:35:21 -0200

    1.6 EXERCÍCIOS: PRIMEIRO SERVIÇO COM NODE

--- ola-server.js
+++ ola-server.js
@@ -4,8 +4,17 @@ const http = require('http'),
 
 http.createServer((requisicao, resposta) => {
         console.log(`Request recebida!`)
-        resposta.writeHead(200, {'Content-Type': 'text/plain'})
-        resposta.end(`Hello World!`);
+        resposta.writeHead(200, {'Content-Type': 'text/html'})
+        resposta.end(`<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <title>Olá mundo</title>
+</head>
+<body>
+        <h1>Meu servidor Node <3</h1>
+</body>
+</html>`);
 }).listen(porta, ip)
 
 console.log(`Servidor web executando em http://${ip}:${porta}`)
