e69c1ec38fc496e5d7b1965098006bea0550ed99
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-12-26 18:01:03 -0200

    1.12 EXERCÍCIOS: CRIANDO UMA PÁGINA COM O EJS

--- casadocodigo/app.js
+++ casadocodigo/app.js
@@ -1,11 +1,13 @@
 const express = require('express'),
       app = express()
 
+app.set('view engine','ejs')
+
 let server = app.listen(3000, () => {
     console.log(`Servidor rodando em http://localhost:3000`)
 })
 
 app.get('/produtos',(requisicao, resposta) => {
-    resposta.send(`<h1>Listagem de produtos</h1>`)
+    resposta.render(`produtos/lista`)
 })
 
