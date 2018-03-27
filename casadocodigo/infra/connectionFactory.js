21702336a945d395f164890a49a67deef4ea587f
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-03-27 16:06:34 -0300

    2.6 Factory Method - isolando a conexao

---
+++ casadocodigo/infra/connectionFactory.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql')
+
+function createConnection() {
+
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'casadocodigo'
+    })
+    
+}
+
+module.exports = createConnection
